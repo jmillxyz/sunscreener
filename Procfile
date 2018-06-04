8e27a1f3898d37d60a04d8d221298f589158338e
Jon Miller <jondelmil@gmail.com>
2018-06-04 10:11:53 -0500

    Heroku procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn sunscreener:app --log-file -
--- sunscreener.py
+++ sunscreener.py
@@ -5,3 +5,10 @@ class SunScreenerResource:
     def on_get(eslf, req, resp):
         resp.status = falcon.HTTP_200
         resp.body = ()
+
+
+app = falcon.API()
+
+sunscreener = SunScreenerResource()
+
+app.add_route('/', sunscreener)
